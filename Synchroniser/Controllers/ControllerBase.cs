23daa1896bb0272f816a54aa807c820ad07d9b3a
Jianfeng Li <jianfeng.li@ersa.edu.au>
2018-02-27 16:48:37 +1030

    Add Controller base for better setup logger and client
    Could not create private Entity in controller using generic

---
+++ Synchroniser/Controllers/ControllerBase.cs
@@ -0,0 +1,20 @@
+﻿//using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Client;
+using System;
+
+namespace Synchroniser.Controllers
+{
+    public class ControllerBase<UController> : Controller where UController : Controller
+    {
+        protected readonly ILogger<UController> _logger;
+        protected readonly ITokenConsumer _crmClient;
+
+        public ControllerBase(ILogger<UController> logger, ITokenConsumer crmClient)
+        {
+            _logger = logger;
+            _crmClient = crmClient;
+        }
+    }
+}
---
+++ Synchroniser/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Client;
+
+
+namespace Synchroniser.Controllers
+{
+    public class OrderController : ControllerBase<OrderController>
+    {
+        private Client.Entities.Order order;
+
+        public OrderController(ILogger<OrderController> logger, ITokenConsumer crmClient) : base(logger, crmClient)
+        {
+            order = new Client.Entities.Order((CRMClient)crmClient);
+        }
+
+        public IActionResult Get()
+        {
+            return Ok("this is good");
+        }
+    }
+}
